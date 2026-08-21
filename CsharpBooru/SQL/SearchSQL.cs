a9b5dcb3862b1317ef968be5a98043612d539a2f
broumbroum <broumbroum2002@gmail.com>
2026-08-21 23:38:35 +0200

    Enhance search functionality: add support for file extensions

--- CsharpBooru/SQL/SearchSQL.cs
+++ CsharpBooru/SQL/SearchSQL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace CsharpBooru.SQL;
@@ -7,11 +8,12 @@ internal class SearchSQL {
 
 	public static string querySearch = "";
 
-	public static (List<string> include, List<string> exclude, List<string> ratings) ParseSearch (string input) {
-		List<string>? 
+	public static (List<string> include, List<string> exclude, List<string> ratings, List<string> extension) ParseSearch (string input) {
+		List<string>?
 			include = [],
 			exclude = [],
-			ratings = [];
+			ratings = [],
+			extension = [];
 
 		var parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
@@ -20,18 +22,22 @@ internal class SearchSQL {
 				var r = p["rating:".Length..].Trim();
 				if (!string.IsNullOrWhiteSpace(r))
 					ratings.Add(r);
-			} else if (p.StartsWith('-'))
+			}else if (p.StartsWith("extension:", StringComparison.OrdinalIgnoreCase)) {
+				var r = p["extension:".Length..].Trim();
+				if (!string.IsNullOrEmpty(r))
+					extension.Add(r);
+			}else if (p.StartsWith('-'))
 				exclude.Add(p[1..]);
 			else
 				include.Add(p);
 		}
 
-		return (include, exclude, ratings);
+		return (include, exclude, ratings, extension);
 	}
 
 	public static List<int> SearchPosts (string query) {
 		if (query == null || query == "") return [.. PostsManager.GetAllPosts().Select(p => p.Id)];
-		var (includeNames, excludeNames, ratingFilters) = ParseSearch(query);
+		var (includeNames, excludeNames, ratingFilters, extensionFilters) = ParseSearch(query);
 
 
 		var includeIds = includeNames
@@ -47,6 +53,14 @@ internal class SearchSQL {
 		Post post;
 		foreach (var idP in PostsManager.GetAllPostIds()) {
 			post = PostsManager.GetPost(idP);
+
+			foreach (var extensionFilter in extensionFilters) {
+				if (Path.GetExtension(post.Filename) == ("." + extensionFilter))
+					continue;
+				else
+					goto NextPost;
+			}
+
 			foreach (var includeId in includeIds) {
 				if (includeId == -1)
 					goto NextPost;
